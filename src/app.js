54c4ebf1116284de1eef1bdd1d3779948736b407
دابس بالعربي <arabicdapps@gmail.com>
2024-07-08 21:03:56 +0300

    solve bootstrap error

--- src/app.js
+++ src/app.js
@@ -1,26 +1,25 @@
 //import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/js/bootstrap"; // Example import path
 
+import Navbar from "./Navbar.js";
 
-import Navbar from './Navbar.js';
-
-import Posts from './Employees.js';
+import Posts from "./Employees.js";
 
 function App() {
-
-  const thename="Mutaher";
-  const theage=45;
+  const thename = "Mutaher";
+  const theage = 45;
 
   return (
     <div className="App">
-     <Navbar /> 
-     <Posts />     
-  {/*    <Navbar />  
+      <Navbar />
+      <Posts />
+      {/*    <Navbar />  
       <Home />
       <h1> {thename} </h1>
       <h3> {theage} </h3>
       <About />
-    */ } 
+    */}
     </div>
   );
 }
